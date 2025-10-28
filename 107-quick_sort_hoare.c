4a62bd400c1f98ecc3fccd42636fdc819044190c
root <Nada.Ghannam19@gmail.com>
2025-10-28 13:45:32 +0000

    Implement quick sort using Hoare partition scheme and Big O file

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+* swap_ints - swaps two integers
+* @a: first integer
+* @b: second integer
+*/
+void swap_ints(int *a, int *b)
+{
+int tmp = *a;
+*a = *b;
+*b = tmp;
+}
+
+/**
+* hoare_partition - implements Hoare partition scheme
+* @array: array of integers
+* @size: size of array
+* @low: starting index
+* @high: ending index
+* Return: partition index
+*/
+int hoare_partition(int *array, size_t size, int low, int high)
+{
+int pivot = array[high];
+int i = low - 1, j = high + 1;
+
+while (1)
+{
+do {
+i++;
+} while (array[i] < pivot);
+
+do {
+j--;
+} while (array[j] > pivot);
+
+if (i >= j)
+return (j);
+
+swap_ints(&array[i], &array[j]);
+print_array(array, size);
+}
+}
+
+/**
+* quick_sort_hoare_rec - recursive quick sort using Hoare partition
+* @array: array of integers
+* @size: size of array
+* @low: starting index
+* @high: ending index
+*/
+void quick_sort_hoare_rec(int *array, size_t size, int low, int high)
+{
+int p;
+
+if (low < high)
+{
+p = hoare_partition(array, size, low, high);
+quick_sort_hoare_rec(array, size, low, p);
+quick_sort_hoare_rec(array, size, p + 1, high);
+}
+}
+
+/**
+* quick_sort_hoare - sorts an array using Quick sort (Hoare scheme)
+* @array: array to sort
+* @size: size of array
+*/
+void quick_sort_hoare(int *array, size_t size)
+{
+if (!array || size < 2)
+return;
+
+quick_sort_hoare_rec(array, size, 0, size - 1);
+}
+
